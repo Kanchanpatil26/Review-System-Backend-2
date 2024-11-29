031f07572f89adcc27e99ee10e089f6655f9f09b
Kanchan Patil <161240351+Kanchanpatil26@users.noreply.github.com>
2024-11-29 23:08:55 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use("/api/v1",router)
 
 app.get("/", (req, res)=>{
-  res.json({
+  res.send({
     success: true,
-    message: "Server Running Successfully"
+    message: "Review System"
   })
 })
 
